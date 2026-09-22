12fb7676665dd4648884bacdf15164355cd2efda
Aadarsh Shrivastav <aadarshshrivastav01@gmail.com>
2026-09-22 21:47:08 +0530

    ratelimiters

--- backend/src/middleware/rateLimiter.js
+++ backend/src/middleware/rateLimiter.js
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 export const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 100,
+  max: 2000,
   standardHeaders: true,
   legacyHeaders: false,
   message: { success: false, message: "Too many requests, please try again later" },
